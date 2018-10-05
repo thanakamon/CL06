343c38b2793abfc1f95ab7316b9d3facd79d9775
thanakamon <thanakamonmay@gmail.com>
2018-10-05 14:41:46 +0700

    7

--- CL06/CL06/Program.cs
+++ CL06/CL06/Program.cs
@@ -4,20 +4,20 @@ class Program
 {
     static void Main()
     {
-        int b = add(1, 2, 3, 4, 5);
-        Console.WriteLine("b = {0}", b);
-        Console.WriteLine("1+5+8+4+1+6+6+8 = " + add(1, 5, 8, 4, 1, 6, 6, 8));
+        object[] myObjArray = { 2, 'b', 1000.05d, "Hello World" };
+        useParams(myObjArray);
+        useParams("IE.Tech.", "KMITL", 2018);
         Console.ReadLine();
     }
 
-    static int add(params int[] i)
+    static void useParams(params object[] list)
     {
-        int sum = 0; ;
-        foreach (int j in i)
-            sum += j;
-        return sum;
+        foreach (object o in list)
+            Console.Write(o + " ");
+        Console.WriteLine();
     }
 }
+
 
 
 
