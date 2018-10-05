4bc91d106944048a8b1de278e4e6dba2966357bb
thanakamon <thanakamonmay@gmail.com>
2018-10-05 13:53:10 +0700

    3

--- CL06/CL06/Program.cs
+++ CL06/CL06/Program.cs
@@ -4,14 +4,13 @@ class Program
 {
     static void Main()
     {
-        sayHello();
+        sayHello("Hello World.");
         Console.ReadLine();
     }
 
-    static void sayHello()
+    static void sayHello(string str)
     {
-        Console.WriteLine("Hello World.");
+        Console.WriteLine(str);
     }
 }
-
 
