53d18497a8625d5d1b477339c0688d02052c5dc4
thanakamon <thanakamonmay@gmail.com>
2018-10-05 16:16:34 +0700

    12

--- CL06/CL06/Program.cs
+++ CL06/CL06/Program.cs
@@ -4,22 +4,17 @@ class Program
 {
     static void Main()
     {
-        int x = 17;
-        double y = x / 4.0;
-        double z = y * y;
-        Console.WriteLine(y);
+        int x = 6;
+        int y = 4;
+
+        Console.WriteLine(y *= 2);
+        int z = y = 3;
+
         Console.WriteLine(z);
-
-        int a = 5;
-        int b = 4;
-        Console.WriteLine(a + b);
-        Console.WriteLine(a + (b++));
-        Console.WriteLine(a + b);
-        Console.WriteLine(a + (++b));
-        Console.WriteLine(a + b);
-        Console.WriteLine(14 / a);
-        Console.WriteLine(14 % a);
-
+        Console.WriteLine(x |= 1);
+        Console.WriteLine(x += 3);
+        Console.WriteLine(x /= 2);
+        Console.WriteLine(x + = 3);
     }
 }
 
