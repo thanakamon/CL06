dc7ca66f90c1a7d9cf71dde195030c349cda76fa
thanakamon <thanakamonmay@gmail.com>
2018-10-06 12:49:14 +0700

    14

--- CL06/CL06/Program.cs
+++ CL06/CL06/Program.cs
@@ -4,9 +4,13 @@ class Program
 {
     static void Main()
     {
-        double GPA = 1.95;
-        Console.WriteLine("You are " + (GPA >= 2.0 ? "pass" : "fall"));
-
+        int x = 10, y = 5;
+        Console.WriteLine("x > y : " + (x > y));
+        Console.WriteLine("x < y : " + (x < y));
+        Console.WriteLine("x >= y : " + (x >= y));
+        Console.WriteLine("x <= y : " + (x <= y));
+        Console.WriteLine("x == y : " + (x == y));
+        Console.WriteLine("x != y : " + (x != y));
 
     }
 }
